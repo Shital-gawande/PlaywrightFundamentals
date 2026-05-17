e725568195e432e22b73369b8245c70345c65c61
Shital Gawande <shitalgawande01@gmail.com>
2026-05-17 15:48:08 +0530

    Adding WebTable code

---
+++ tests/Ch07_WebTables/WebTable_NativeWay.spec.ts
@@ -0,0 +1,20 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('Webtable handling playwright way', ()=>{
+
+    test('Webtable handling', async ({page} ) =>{
+
+        await page.goto('https://awesomeqa.com/webtable1.html');
+
+        const rows = page.locator('table[summary="Sample Table"] tbody tr');
+
+        const rowCount = await rows.count();
+
+        for(let i= 1;i<=rowCount; i++){
+
+           const data = await rows.nth(i).locator('td').allInnerTexts();
+           console.log(`Row ${i} data is - ${data}`);
+        }
+
+    });
+});
